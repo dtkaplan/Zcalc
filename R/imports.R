5c8da1f04f815e77f0c06449fdbdde4ebfb023a4
Danny Kaplan <kaplan@macalester.edu>
2021-09-01 18:54:14 -0500

    Re-exported a few data sets from MMAC

--- R/imports.R
+++ R/imports.R
@@ -3,3 +3,18 @@
 #' @import palmerpenguins
 #' @export
 penguins <- palmerpenguins::penguins
+#'
+#' @import MMAC
+#' @export
+BlastData <- MMAC::BlastData
+#' @export
+SunsetLA <- MMAC::SunsetLA
+#' @export
+EbolaSierraLeone <- MMAC::EbolaSierraLeone
+#' @export
+RunningSpeed <- MMAC::RunningSpeed
+#' @export
+SwimmingSpeed <- MMAC::SwimmingSpeed
+
+
+

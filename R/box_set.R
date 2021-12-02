863cc9094b1c8ebac58a4fc7e662c4d7ba6cc6a0
Danny Kaplan <kaplan@macalester.edu>
2021-12-02 08:22:26 -0600

    Adding shapes

--- R/box_set.R
+++ R/box_set.R
@@ -5,7 +5,7 @@
 #' each of which can be considered to correspond to a Riemann bin
 #' for the purposes of integration.
 #'
-#' @value By default, a data frame listing the location of the samples,
+#' @return By default, a data frame listing the location of the samples,
 #' the `.output.` value of the given function at those samples, the spatial
 #' extent of the samples (that is, dx for one-dimension or dx and dy for two dimensions. There
 #' is also a `dA` giving the dx or dx*dy depending on the dimension). If `sum=TRUE`,
@@ -19,20 +19,22 @@
 #' @param n the number of divisions along each of the x- and y-directions. Can
 #' be a vector of length 2 giving different numbers for the x and for the y directions.
 #' @param sum If `TRUE` carry out the integral and return the numerical result.
+#' @param dx An alternative way of specifying the box size directly. Same
+#' for all dimensions.
 #' @examples
 #' box_set(x*y ~ x & y, domain(x=0:1, y=0:1), n = 4)
 #' # approximation to the variance of a uniform [0,1] distribution
 #' box_set((x-.5)^2 ~ x, domain(x=0:1), n=100, sum=TRUE)
 #' # a polygon
 #' poly <- tibble(x = c(1:9, 8:1), y = c(1, 2*(5:3), 2, -1, 17, 9, 8, 2:9))
-#' boxes <- box_set(1 ~ x & y, poly)
-#' gf_polygon(y ~ x, data = poly) %>%
-#'   gf_rectangle((y - dy/3) + (y + dy/3) ~ (x - dx/3) + (x + dx/3),
+#' boxes <- box_set(1 ~ x & y, poly, dx = 1)
+#' gf_polygon(y ~ x, data = poly, color="blue", fill="blue", alpha=0.2) %>%
+#'   gf_rect((y - dy/3) + (y + dy/3) ~ (x - dx/3) + (x + dx/3),
 #'   data = boxes)
 #' # area inside polygon
 #' box_set(1 ~ x & y, poly, n=100)
 #' @export
-box_set <- function(tilde = NULL, domain, n=10, sum=FALSE) {
+box_set <- function(tilde = NULL, domain, n=10, sum=FALSE, dx=NULL) {
   # can give either a regular domain or a polygon
   if (is.data.frame(domain)) {
     polygon <- domain
@@ -55,7 +57,8 @@ box_set <- function(tilde = NULL, domain, n=10, sum=FALSE) {
   if (!is.list(domain)) stop("Domain must be a named list, as in Integrate() or contour_plot().")
   nms <- names(domain)
   if (length(domain) == 1) {
-    dx <- diff(range(domain[[1]])) / n
+    if (is.null(dx))
+      dx <- diff(range(domain[[1]])) / n
     res <- tibble(
       x = seq(min(domain[[1]]) + dx/2, max(domain[[1]]), by = dx),
       dx = dx,
@@ -65,8 +68,14 @@ box_set <- function(tilde = NULL, domain, n=10, sum=FALSE) {
 
   } else if (length(domain) == 2) {
     if (length(n) == 1) n <- c(n, n)
-    dx <- diff(range(domain[[1]])) / n[1]
-    dy <- diff(range(domain[[2]])) / n[2]
+    if (is.null(dx)) {
+      dx <- diff(range(domain[[1]])) / n[1]
+      dy <- diff(range(domain[[2]])) / n[2]
+    } else {
+      if (length(dx) == 1) dx <- c(dx, dx)
+      dy <- dx[2]
+      dx <- dx[1]
+    }
     x <- seq(min(domain[[1]]) + dx/2, max(domain[[1]]), by = dx)
     y <- seq(min(domain[[2]]) + dy/2, max(domain[[2]]), by = dy)
     res <- expand.grid(list(x=x, y=y)) %>%

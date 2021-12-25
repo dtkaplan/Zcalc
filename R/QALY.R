18533e74db947de9651a4ea9efb91264041b5294
Danny Kaplan <kaplan@macalester.edu>
2021-12-25 05:04:36 -0600

    Added small QALY dataset

---
+++ R/QALY.R
@@ -0,0 +1,15 @@
+#' Quality Adjusted Life Years
+#'
+#' A small, made-up data set used in an exercise about optimizing
+#' health-care expenditures.
+#'
+#' @export
+QALY_impact <- tibble::tribble(
+  ~expend, ~A, ~ B,
+  0, 0, 0,
+  10, 40, 30,
+  20, 100, 80,
+  30, 150, 140,
+  40, 170, 160,
+  50, 180, 175
+)

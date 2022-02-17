a5651e7db6c8ec7898a25d75a575ae78f7564cea
Danny Kaplan <kaplan@macalester.edu>
2022-02-17 17:30:18 -0600

    adding vowel dataframes

---
+++ R/Vowels.R
@@ -0,0 +1,13 @@
+#' Recordings of vowel sounds
+#'
+#' "Oh" refers to "o" as in "stone." "Ee" to "eel".
+#'
+#'
+#'
+#' - `Oh_sound` and `Ee_sound` contain the complete utterances.
+#' - `Vowel_ee` and `Vowel_oh` are snippets of about 0.04 seconds.
+#'
+#'
+#'
+"Vowel_ee"
+

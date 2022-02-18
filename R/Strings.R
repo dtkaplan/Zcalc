1ba4b8098ed99754907b3fc969c743d3e47c4c7d
Danny Kaplan <kaplan@macalester.edu>
2022-02-18 17:07:33 -0600

    Adding cello and violin sounds

---
+++ R/Strings.R
@@ -0,0 +1,13 @@
+#' Recordings of musical notes
+#'
+#' Cello and violin notes. Data frames with `t` and `y` columns
+#'
+#' `Cello` and `Violin` are complete notes
+#'
+#' `Cello_seg` and `Violin_seg` are extracts, approximately 90 ms long
+#'
+#' Source: https://philharmonia.co.uk/resources/sound-samples/
+#'
+#'
+"Cello"
+
--- R/Vowels.R
+++ R/Vowels.R
@@ -6,6 +6,7 @@
 #'
 #' - `Oh_sound` and `Ee_sound` contain the complete utterances.
 #' - `Vowel_ee` and `Vowel_oh` are snippets of about 0.04 seconds.
+#' - `A_pitch` (https://upload.wikimedia.org/wikipedia/commons/5/50/Sine_wave_440.ogg)
 #'
 #'
 #'
